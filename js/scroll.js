1a9885ee9f293fc1bb17db8b576751d36cbb466f
ElenaKiseljova <e.a.kiseljova@gmail.com>
2022-02-01 13:25:44 +0200

    - wrapped scroll function

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,12 @@
-$(window).scroll(function () {
-  const footer = document.querySelector(".footer").offsetHeight;
-  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-    document.querySelector(
-      ".shop__bag"
-    ).style.transform = `translateY(${-footer}px)`;
-  } else {
-    document.querySelector(".shop__bag").style.transform = `translateY(0)`;
-  }
-});
+(function ($) {
+  $(window).scroll(function () {
+    const footer = document.querySelector(".footer").offsetHeight;
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+      document.querySelector(
+        ".shop__bag"
+      ).style.transform = `translateY(${-footer}px)`;
+    } else {
+      document.querySelector(".shop__bag").style.transform = `translateY(0)`;
+    }
+  });
+})(jQuery)
