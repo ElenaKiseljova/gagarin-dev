a593e288ca291a536fc3986b985274916d805d57
codeurient <codeurient@gmail.com>
2022-02-04 13:42:28 +0200

    правки поправлены

--- js/curtains.js
+++
@@ -1,7 +0,0 @@
-const curtains = document.querySelector(".curtains");
-curtains.addEventListener("mouseenter", () => {
-  curtains.classList.add("active");
-});
-curtains.addEventListener("mouseleave", () => {
-  curtains.classList.remove("active");
-});
