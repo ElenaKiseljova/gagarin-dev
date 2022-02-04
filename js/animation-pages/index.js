a5dbb15528064ccc0adf852644027ffe4c86db87
codeurient <codeurient@gmail.com>
2022-02-04 14:53:51 +0200

    no res

--- js/animation-pages/index.js
+++ js/animation-pages/index.js
@@ -1,14 +1,14 @@
 animateFadeOutBottom(".intro__inner", ".intro__inner *");
 animateFadeOutBottom(".products__grid", ".products__grid > *");
 animateFadeOutBottom(".filter__left-box", ".filter__left-box li ");
-animateFadeOutBottom(".about__title", ".about__title span");
 animateFadeOutBottom(".list", ".list__item");
 animateOpacity(".statistics__box", ".statistics__item-wrapper");
 animateFadeOutBottomS(".message__form", ".message__form > *");
 
+animateFadeOutBottom(".about__title", ".about__title span");
 animateFadeOutBottom(".about__content", ".about__content p");
-animateFadeOutBottomS(".equipment-rental", ".equipment-rental__left *");
 animateFadeOutBottom(".about__list", ".about__list *");
+animateFadeOutBottomS(".equipment-rental", ".equipment-rental__left *");
 
 if (document.body.offsetWidth > 768) {
   const items = document.querySelectorAll(".statistics__item-wrapper");
