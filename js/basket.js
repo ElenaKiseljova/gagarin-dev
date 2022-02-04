9d5ab4ecf4cb88c9ec048ebc7b8e58b80dcd3203
Vlad <60440826+Vladik-web@users.noreply.github.com>
2022-02-04 14:08:44 +0200

    Update basket.js

--- js/basket.js
+++ js/basket.js
@@ -46,13 +46,4 @@ const appHeight = () => {
 window.addEventListener("resize", appHeight);
 appHeight();
 
-/* при нажатии на один из них будет сворачиватся меню */
-const mass = document.querySelectorAll(".contact-btn");
-if (mass) {
-  mass.forEach((item) => {
-    item.addEventListener("click", () => {
-      mobileNav.classList.remove("--show");
-      document.body.classList.remove("active");
-    });
-  });
-}
+
